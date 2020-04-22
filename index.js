3855ee04152ce10f353ff14a602da0fce30dd491
Your Name <you@example.com>
2020-04-22 16:18:47 +0200

    revert

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ shareButton = document.getElementById("shareButton");
 
 shareButton.addEventListener("click", async() => {
     try {
-        await navigator.share({ title: "Example Page", url: "" });
+        await navigator.share({ title: "Example Page", text: 'Check out web.dev.', url: "example.com" });
         console.log("Data was shared successfully");
     } catch (err) {
         console.error("Share failed:", err.message);
